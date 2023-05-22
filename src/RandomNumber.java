cb7b4dc3d6dd4fd6c41ba02abe0c9b001792b4f9
SteinvB <132998970+SteinvB@users.noreply.github.com>
2023-05-22 14:52:35 +0200

    Delete RandomNumber.java

--- src/RandomNumber.java
+++
@@ -1,8 +0,0 @@
-import java.util.concurrent.ThreadLocalRandom;
-
-public class RandomNumber {
-    public RandomNumber() {
-        int Num = ThreadLocalRandom.current().nextInt(1,1000);
-        System.out.println(Num);
-    }
-}
